52414019f5ce1b73b4313d8579cb981fca4f9a9a
VTTNHEEE <VTTNHEEE@gmail.com>
2019-05-09 21:02:06 +0100

    Team Info Update
    Added The Event Info and Member circles in Teams

---
+++ js/main.js
@@ -0,0 +1,51 @@
+console.log("LOADED")
+
+let circles = document.getElementById("circles")
+
+console.log(info)
+
+info = info.slice(1,info.length-1)
+
+for(let line of info.split("\n")){
+  if(line.startsWith("-TEAM ")){
+    let teamNameDiv = document.createElement("div")
+    let teamName = line.split("-TEAM ")[1]
+    teamNameDiv.className = "teamName"
+    teamNameDiv.innerText = teamName.toUpperCase()
+    if(teamName == "NONE"){
+      teamNameDiv.style.color = "rgba(0,0,0,0)"
+      teamNameDiv.style.margin = 0;
+    }
+    if(teamName == "OTHERS"){
+      teamNameDiv.innerText = "- - - - - - - - - - - - - - - - - - - - - -"
+      teamNameDiv.style["text-decoration"] = "none"
+    }
+    else{
+
+    }
+
+    circles.appendChild(teamNameDiv)
+    continue
+  }
+  let [initials, colour, paid] = line.split(" ")
+
+  if(colour == "none"){
+    colour == "#bbb"
+  }
+  let s = document.createElement("span")
+  if(initials=="??"){
+    s.style.color = "rgba(0,0,0,0)"
+    s.style["border-style"] = "dashed"
+  }
+
+  s.className = "circle";
+  s.style["background-color"] = colour;
+  s.innerText = initials
+  if(paid == "P"){
+    s.style["border-style"] = "solid";
+  }
+  else{
+    s.style["border-style"] = "dashed";
+  }
+  circles.appendChild(s)
+}
