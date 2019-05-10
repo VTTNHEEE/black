1ba6bc63706d7b0acdbe9e83f9f49d3b53dc7053
VTTNHEEE <49096905+VTTNHEEE@users.noreply.github.com>
2019-05-10 15:49:29 +0100

    Updated Team Claire and Team Howe

--- js/members.js
+++ js/members.js
@@ -5,7 +5,14 @@ BG SandyBrown X
 MO SandyBrown X
 AC SandyBrown X
 LF SandyBrown X
-A- SandyBrown X
+AK SandyBrown X
+-TEAM Generic team name
+CH mediumpurple P
+LD mediumpurple X
+TS mediumpurple P
+LD mediumpurple X
+PB mediumpurple X
+BJ mediumpurple X
 -TEAM TEAM 2
 RA hotpink X
 KM hotpink X
@@ -13,21 +20,14 @@ GF hotpink X
 EH hotpink X
 LM hotpink X
 MH hotpink X
+-TEAM TEAM HOWE
+MH firebrick X
+SM firebrick P
+DC firebrick X
+MN firebrick X
+CA firebrick X
+EL firebrick X
 -TEAM OTHERS
-SM gray P
-CH gray P
-TS gray P
-?? black X
-?? black X
-?? black X
--TEAM NONE
-?? black X
-?? black X
-?? black X
-?? black X
-?? black X
-?? black X
--TEAM NONE
 ?? black X
 ?? black X
 ?? black X
