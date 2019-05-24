69aa530fde4b17452b5243bcd2fc4428a16d7eaf
VTTNHEEE <49096905+VTTNHEEE@users.noreply.github.com>
2019-05-24 19:01:51 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 console.log("LOADED");
 
-let circles = document.getElementById("circles");
+var circles = document.getElementById("circles");
 info = info.slice(1,info.length-1);
 
-for(let line of info.split("\n")){
+var lines = info.split("\n");
+
+for(var i = 0; i < lines.length; i++){
+  var line = lines[i];
   if(line.startsWith("-TEAM ")){
-    let teamNameDiv = document.createElement("div");
-    let teamName = line.split("-TEAM ")[1]
+    var teamNameDiv = document.createElement("div");
+    var teamName = line.split("-TEAM ")[1]
     teamNameDiv.className = "teamName";
     teamNameDiv.innerText = teamName.toUpperCase();
     if(teamName == "NONE"){
@@ -21,8 +24,8 @@ for(let line of info.split("\n")){
     continue;
   }
   
-  let [initials, colour, paid] = line.split(" ");
-  let s = document.createElement("span");
+  var [initials, colour, paid] = line.split(" ");
+  var s = document.createElement("span");
   
   if(initials=="??"){
     s.style.color = "rgba(0,0,0,0)";
